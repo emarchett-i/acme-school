aa0f56c5c47c54ec533866441d9a4623e4c29290
emarchett-i <enrique.matt1@gmail.com>
2024-07-29 16:36:09 -0300

    add test for RegisterStudent use case

---
+++ Acme.School.Application.Tests/CommandHandlers/RegisterStudentCommandHandlerTests.cs
@@ -0,0 +1,50 @@
+﻿using Acme.School.Application.Students.Commands;
+using Acme.School.Application.Students.Handlers;
+using Acme.School.Domain.Entities;
+using Acme.School.Domain.Interfaces;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Moq;
+using Moq.AutoMock;
+
+namespace Acme.School.Application.Tests.CommandHandlers
+{
+    public class RegisterStudentCommandHandlerTests
+    {
+        private RegisterStudentCommandHandler _handler;
+        private AutoMocker _autoMocker;
+
+        public RegisterStudentCommandHandlerTests()
+        {
+            _autoMocker = new AutoMocker();
+            _handler = _autoMocker.CreateInstance<RegisterStudentCommandHandler>();
+        }
+
+        [Fact]
+        public async Task Handle_ShouldAddNewUser()
+        {
+            //Arrange
+            var command = new RegisterStudentCommand("name", 21);
+            Student? studentBeingCreated = null;
+            _autoMocker.GetMock<IStudentRepository>()
+                .Setup(x => x.Add(It.IsAny<Student>(), It.IsAny<CancellationToken>()))
+                .Callback<Student, CancellationToken>((student, cancellationToken) => { studentBeingCreated = student; });
+
+            //Act
+            await _handler.Handle(command, CancellationToken.None);
+
+            //Assert
+            _autoMocker.GetMock<IStudentRepository>()
+                .Verify(x => 
+                    x.Add(It.IsAny<Student>(), It.IsAny<CancellationToken>()), 
+                    Times.Once);
+
+            studentBeingCreated.Should().NotBeNull();
+            using (new AssertionScope())
+            {
+                studentBeingCreated.Name.Should().Be(command.Name);
+                studentBeingCreated.Age.Should().Be(command.Age);
+            }
+        }
+    }
+}
